a76dba8b95bbf36f424d630d1be97da7a1f93523
sadness_ojisan <sadness.ojisan@gmail.com>
2020-08-04 15:31:10 +0900

    add key

---
+++ next.config.js
@@ -0,0 +1,7 @@
+console.log("[next.config.js] process.env", process.env);
+
+module.exports = {
+  env: {
+    customKey: "my-value",
+  },
+};
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 
 export default function App({ Component, pageProps }) {
-  console.log("[app.jsx] process.env", process.env);
+  console.log("[app.jsx] process.env.customKey", process.env.customKey);
+  console.log(
+    "[app.jsx] process.env.undefiendedKey",
+    process.env.undefiendedKey
+  );
+  console.log("[app.jsx] process.env.NODE_ENV", process.env.NODE_ENV);
   return <Component {...pageProps} />;
 }
